37bcdd0f69e41cf4efb8358041fa4446c64860e3
zokowvz <88991008+zokowvz@users.noreply.github.com>
2021-08-16 18:08:12 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,52 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.on("ready", () => {
+  console.log("je suis co gro");
+});
+
+client.on("message", (message) => {
+  if (message.content.includes("https://")) {
+    console.log("deleted " + message.content + " from " + message.author)
+    message.delete(1);
+    message.channel.sendMessage("pas de lien ici, t sah la ?, " + message.author)
+  }
+  if (message.content.includes("http://")) {
+    console.log("deleted " + message.content + " from " + message.author)
+    message.delete(1);
+    message.channel.sendMessage("pas de lien ici, t sah la ?, " + message.author)
+  }
+    if (message.content.includes("discord.gg/")) {
+    console.log("deleted " + message.content + " from " + message.author)
+    message.delete(1);
+    message.channel.sendMessage("pas de lien ici, t sah la ?, " + message.author)
+  }
+  if (message.content.includes("discord.gg")) {
+    console.log("deleted " + message.content + " from " + message.author)
+    message.delete(1);
+    message.channel.sendMessage("pas de lien ici, t sah la ?, " + message.author)
+  }
+  if (message.content.includes("www.")) {
+    console.log("deleted " + message.content + " from " + message.author)
+    message.delete(1);
+    message.channel.sendMessage("pas de lien ici, t sah la ?, " + message.author)
+  }
+  if (message.content.includes("!help")) {
+    console.log("commande help executée par " + message.author)
+   message.channel.send("Panel d'help, fin y'a que l'antilink ^^")
+  }
+  if (message.content.startsWith("!invite")) {
+    console.log("invite par " + message.author)
+    message.channel.sendMessage("discord.gg/lightgen")
+  }
+  if (message.content.startsWith("!ping")) {
+    console.log("ping par " + message.author)
+    message.channel.sendMessage( client.ping + "ms.")
+  }
+});
+
+client.on("guildCreate", guild => {
+  console.log("j'viens de join un serv ptdr, " + guild.name)
+});
+
+client.login("met ton token ici assisté");
