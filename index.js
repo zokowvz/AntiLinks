73bf3da7deaf820302f2aaeaaa3f655e2721124e
zokow <88991008+zokowvz@users.noreply.github.com>
2022-03-27 16:08:45 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
-
-client.on("ready", () => {
-  console.log("[+] connecté à ${client.user.username} ");
-});
-
-client.on("message", (message) => {
-  if (message.content.includes("https://")) {
-    console.log("deleted" + message.content + " from " + message.author)
-    message.delete(1);
-    message.channel.sendMessage("pas de lien ici, t sah la ?, " + message.author)
+var pub = 'discord.gg/' || 'discordapp.com/invite/'||'https://'||'http://' || 'discord.com/invite/' // vous pouvez en ajouter plus si nécessaire.
+client.on('message', (message) => {
+  if (message.content.includes(pub)) {
+    message.delete()
+      .then(message.channel.send("tu ne peux pas envoyer de lien ici !"))
   }
-  if (message.content.includes("http://")) {
-    console.log("deleted " + message.content + " from " + message.author)
-    message.delete(1);
-    message.channel.sendMessage("pas de lien ici, t sah la ?, " + message.author)
-  }
-    if (message.content.includes("discord.gg/")) {
-    console.log("deleted " + message.content + " from " + message.author)
-    message.delete(1);
-    message.channel.sendMessage("pas de lien ici, t sah la ?, " + message.author)
-  }
-  if (message.content.includes("discord.gg")) {
-    console.log("deleted " + message.content + " from " + message.author)
-    message.delete(1);
-    message.channel.sendMessage("pas de lien ici, t sah la ?, " + message.author)
-  }
-  if (message.content.includes("www.")) {
-    console.log("deleted " + message.content + " from " + message.author)
-    message.delete(1);
-    message.channel.sendMessage("pas de lien ici, t sah la ?, " + message.author)
-  }
-  if (message.content.includes("!help")) {
-    console.log("commande help executée par " + message.author)
-   message.channel.send("Panel d'help, fin y'a que l'antilink ^^")
-  }
-  if (message.content.startsWith("!invite")) {
-    console.log("invite par " + message.author)
-    message.channel.sendMessage("discord.gg/lightgen")
-  }
-  if (message.content.startsWith("!ping")) {
-    console.log("ping par " + message.author)
-    message.channel.sendMessage( client.ping + "ms.")
+  if(message.content == "support") {
+    message.channel.send("discord.gg/lightgen")
   }
 });
-
-client.on("guildCreate", guild => {
-  console.log("j'viens de join un serv ptdr, " + guild.name)
-});
-
-client.login("mets ton token ici assisté");
+client.login("token ici");
