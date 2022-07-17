104ca94eaa0fb1fd002501a46939d0a353821bde
zokow <88991008+zokowvz@users.noreply.github.com>
2022-07-17 15:57:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const client = new Discord.Client();
 let prefix = config.prefix
 // NE PAS LANCER ENCORE CAR JE NE L'AI MEME PAS TEST J'LAI FAIT A 4H PSQ J'MENNUYAIS JE N'AI PAS ENCORE FINI !!!
 // la db 
-const {Table, Database} = require('luma-db-v2')
+const {Table, Database} = require("luma-db-v2")
 const whitelist = new Table("wl")
 const link = new Table("links")
 const onoff = new Table("onoff")
-const myDb = new Database({
+const db = new Database({
     name: "dbname", 
     password: "dbpass", 
-    tables: [wl, links, onoff] 
+    tables: [whitelist, link, onoff] 
 })
      // ------------ FIN CONFIG DB
 // pub
-var pub = 'discord.gg/' || 'discordapp.com/invite/'||'https://'||'http://' || 'discord.com/invite/' // vous pouvez en ajouter plus si nécessaire.
+// var pub = 'discord.gg/' || 'discordapp.com/invite/'||'https://'||'http://' || 'discord.com/invite/' // vous pouvez en ajouter plus si nécessaire.
 client.on('message', (message) => {
   let command = message.content
 
@@ -28,40 +28,45 @@ client.on('message', (message) => {
        message.reply("``Tu dois être Propriétaire du bot pour executer cette commande !``")
      }
      if(message.author === config.owner){
-       wl.push('list', args[0]).save()
-       console.log(wl.get('list'))
+       whitelist.push('wl', args[0]).save()
+       console.log(whitelist.get('wl'))
        message.reply("Le membre portant l'id" + "``" + args[0] + "``" + "a été ajouté dans la DB !")
      }
   }
   if(command === prefix + "addlink"){
-      if(message.author !== config.owner){
+    if(!args[0]){
+      message.reply("Je ne peux pas AntiLink le vent !!")
+    }
+      if(message.author.id !== config.owner){
        message.reply("``Tu dois être Propriétaire du bot pour executer cette commande !``")
      }
-  }
-  if(message.author === config.owner){
-       links.set('link', args[0]).save()
-       console.log(links.get('link'))
+  
+  if(message.author.id === config.owner || whitelist.get('wl')){
+       link.set('links', args[0]).save()
+       console.log(link.get('links'))
        message.reply("Le Lien" + "``" + args[0] + "``" + "a été ajouté dans la DB et sera dorénavant pris en compte !")
      }
+  }
    if(command === prefix + "rmwl"){
      if(!args[0]){
        message.reply("Je dois remove le vent ?")
      }
-     let torf = wl.has(args[0])
+     let torf = whitelist.has(args[0])
      if(torf === true){
-     wl.remove('list', args[0]) //reverse of 'push'
+     whitelist.remove('wl', args[0]) 
      }
-     if(torf === false{
+     if(torf === false){
         message.reply("Je ne trouve pas cette personne WL dans la db !")
-   }
+     }
    }
 
 
-if(message.content.includes(links.get('link')))
-  if(links.get('link') === null){
+
+if(message.content.includes(link.get('links')))
+  if(link.get('links') === null){
     return;
   }
-  if(message.author === config.owner || wl.get('list')){
+  if(message.author === config.owner || whitelist.get('list')){
     return;
   }else{
     message.delete()
@@ -71,7 +76,7 @@ if(message.content.includes(links.get('link')))
     //  .then(message.channel.send("tu ne peux pas envoyer de lien ici !"))
   //}
 if(command == prefix + "alllinks"){
- let alllinks = wl.all()
+ let alllinks = whitelist.all()
  if(alllinks === null){
  message.reply("Il n'y a pas de Liens enregistrés dans la DB !")
  }else{
@@ -79,4 +84,13 @@ if(command == prefix + "alllinks"){
 }
 }
 });
+
+   client.on('messageUpdate', (newMessage) => {
+   if(newMessage.content === link.get('links')){
+    message.delete().then(
+      message.channel.send("Le Lien que tu as envoyé n'est pas autorisé sur ce serveur !")
+    )
+   }
+     });
+
 client.login(config.token);
